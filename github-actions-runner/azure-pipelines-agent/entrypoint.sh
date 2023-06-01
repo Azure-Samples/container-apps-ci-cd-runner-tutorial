e049455756d69d5f4f0664109b4be78772b64179
Anthony Chu <anthony@anthonychu.ca>
2023-06-01 15:11:42 -0700

    Add dockerfiles

---
+++ github-actions-runner/azure-pipelines-agent/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+# Retrieve a short lived runner registration token using the PAT
+REGISTRATION_TOKEN="$(curl -X POST -fsSL \
+  -H 'Accept: application/vnd.github.v3+json' \
+  -H "Authorization: Bearer $GITHUB_PAT" \
+  -H 'X-GitHub-Api-Version: 2022-11-28' \
+  "$REGISTRATION_TOKEN_API_URL" \
+  | jq -r '.token')"
+
+./config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
