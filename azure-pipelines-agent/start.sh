e83df5bc2e7fea49e411287f12bbdc7e67591611
Bram <bramvandenklinkenberg@gmail.com>
2023-10-13 09:27:32 +0200

    adjust expression due to -o being obsolescent

--- azure-pipelines-agent/start.sh
+++ azure-pipelines-agent/start.sh
@@ -62,7 +62,7 @@ AZP_AGENT_PACKAGES=$(curl -LsS \
 
 AZP_AGENT_PACKAGE_LATEST_URL=$(echo "$AZP_AGENT_PACKAGES" | jq -r '.value[0].downloadUrl')
 
-if [ -z "$AZP_AGENT_PACKAGE_LATEST_URL" -o "$AZP_AGENT_PACKAGE_LATEST_URL" == "null" ]; then
+if [ -z "$AZP_AGENT_PACKAGE_LATEST_URL" ] || [ "$AZP_AGENT_PACKAGE_LATEST_URL" == "null" ]; then
   echo 1>&2 "error: could not determine a matching Azure Pipelines agent"
   echo 1>&2 "check that account '$AZP_URL' is correct and the token is valid for that account"
   exit 1
