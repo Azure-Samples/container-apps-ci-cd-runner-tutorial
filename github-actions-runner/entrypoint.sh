2fb40c736a03b7fc7c043b07b19bc4d477a79805
Mahinthan <u.mahinthan@gmail.com>
2023-11-23 16:58:01 +0100

    github app related change

--- github-actions-runner/entrypoint.sh
+++ github-actions-runner/entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/sh -l
 
-# Retrieve a short lived runner registration token using the PAT
-REGISTRATION_TOKEN="$(curl -X POST -fsSL \
-  -H 'Accept: application/vnd.github.v3+json' \
-  -H "Authorization: Bearer $GITHUB_PAT" \
-  -H 'X-GitHub-Api-Version: 2022-11-28' \
-  "$REGISTRATION_TOKEN_API_URL" \
-  | jq -r '.token')"
-
-./config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
+./config.sh --url $REPO_URL --token $APP_KEY --app-id $APP_ID --install-id $INSTALLATION_ID --unattended --ephemeral && ./run.sh
